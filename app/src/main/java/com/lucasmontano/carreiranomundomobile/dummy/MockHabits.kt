d67fb76b48dfa91e4ea09d3c21484fdbe77e2f26
Daniele Stein <danieleastein@gmail.com>
2023-06-26 16:46:29 -0300

    Incluído os textos do arquivo Mock para teste na classe MockHabits

--- app/src/main/java/com/lucasmontano/carreiranomundomobile/dummy/MockHabits.kt
+++ app/src/main/java/com/lucasmontano/carreiranomundomobile/dummy/MockHabits.kt
@@ -12,36 +12,43 @@ object MockHabits { //Nosso objeto com os dados mocados para teste
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Read the book",
+      subtitle = "'Como eu era antes de você'",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Walk the dog",
+      subtitle = "Não esquecer a coleirinha",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Do the dishes",
+      subtitle = "Comprar detergente",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Go to the gym",
+      subtitle = "Hoje é seeexta 0/",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Code every day",
+      subtitle = "Com as aulas do Lucas Montana",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Make a cup of coffee",
+      subtitle = "Em uma xícara ou balde",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
       title = "Incluído item 7 na lista!",
+      subtitle = "Consegui o subtítulo",
       isCompleted = false
     ),
   )
