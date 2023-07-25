ee5d4b85af12e9969d1e5bbb886f4389ada01276
Daniele Stein <danieleastein@gmail.com>
2023-07-25 15:56:08 -0300

    Altertado a lista de testes do app Lista

--- app/src/main/java/com/lucasmontano/carreiranomundomobile/dummy/MockHabits.kt
+++ app/src/main/java/com/lucasmontano/carreiranomundomobile/dummy/MockHabits.kt
@@ -16,49 +16,42 @@ object MockHabits { //Nosso objeto com os dados mocados para teste
   val habitItemList: MutableList<HabitItem> = mutableListOf(  //Lista mutável de habitos, ela recebe a nossa classe habitList
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Read the book",
       subtitle = "'Como eu era antes de você'",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Walk the dog",
       subtitle = "Não esquecer a coleirinha",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Do the dishes",
       subtitle = "Comprar detergente",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Go to the gym",
       subtitle = "Hoje é seeexta 0/",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Code every day",
       subtitle = "Com as aulas do Lucas Montano",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Make a cup of coffee",
       subtitle = "Em uma xícara ou balde",
       isCompleted = false
     ),
     HabitItem(
       id = UUID.randomUUID().toString(),
-      switch = false,
       title = "Incluído item 7 na lista!",
       subtitle = "Consegui o subtítulo",
       isCompleted = false
