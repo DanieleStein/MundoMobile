7e70d67962e97d294c6d945b53aa9e420d5e3f50
Daniele Stein <danieleastein@gmail.com>
2023-08-09 23:25:43 -0300

    Classe para o state da nossa tela de lista de hábitos

---
+++ app/src/main/java/com/lucasmontano/carreiranomundomobile/collections/HabitListUiState.kt
@@ -0,0 +1,5 @@
+package com.lucasmontano.carreiranomundomobile.collections
+
+//classe para o estado da nossa tela de lista
+
+data class HabitListUiState(val habitItemList: List<HabitItem>) //Nosso estado vai ter uma propriedade que é a nossa lista de hábitos(List<HabitItem>) HabitItem(conta as várias que compoem a nossa lista de hábitos)
