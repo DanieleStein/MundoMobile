73f881903a6c14d06748b3e402947097ed230730
Daniele Stein <danieleastein@gmail.com>
2023-08-14 14:43:41 -0300

    Adicionando a função que vai observar o nosso state e vai informar até quando observar

--- app/src/main/java/com/lucasmontano/carreiranomundomobile/collections/HabitListFragment.kt
+++ app/src/main/java/com/lucasmontano/carreiranomundomobile/collections/HabitListFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.divider.MaterialDividerItemDecoration
 import com.lucasmontano.carreiranomundomobile.R
@@ -22,6 +23,10 @@ class HabitListFragment : Fragment() {
   private val binding get() = _binding!!
 
   private lateinit var adapter: HabitListAdapter //o adapter é uma variavel que vamos inicializar depois(laiteinit)
+
+  private val viewModel: HabitListViewModel by activityViewModels { //activityViewModels seja criada e essa instancia é criada e armazenada no escopo da activity, então se qualquer fragmemt dentro da ctivity pedir viewModel do tipo HabitListViewModel o mesmo viewModel vai ser fornecido
+    HabitListViewModel.Factory(MockHabits)                                      //Porque a activity precisa ter acesso a essa mesma instancia do ViewModel
+  }
 
   override fun onCreate(savedInstanceState: Bundle?) { //Vai ser chamado automaticamente pelo android quando o fragment for criado
     super.onCreate(savedInstanceState) //Nesse momento a View do Layout onde está o RecyclerView, ainda não foi inflada com os dados
@@ -46,7 +51,16 @@ class HabitListFragment : Fragment() {
     addingDividerDecoration() //Adicionando o tipo de layout que quero de espaçamento entre os itens da lista
 
     // Updating the list of habits (Fornecendo nossa lista de hábitos para o adapter em uma função chamada updateHabits
-    adapter.updateHabits(MockHabits.habitItemList)
+    //adapter.updateHabits(MockHabits.habitItemList)  - Desabilitando pois nosso fragment não vai mais observar direto do MockHabits, e sim do ViewModel
+    viewModel                      //Então agora vamos pedir para o viewModel
+      .stateOnceAndStream()        //observar o nosso state
+      .observe(viewLifecycleOwner) { state ->   //chamando o metodo observe, e informando até quando ele deve observar (viewLifecycleOwner)enquanto a view existir
+        bindUiState(state)
+      }
+  }
+
+  private fun bindUiState(state: HabitListUiState) { //função vai receber o state
+    adapter.updateHabits(state.habitItemList)        //vai fazer a atualização no adapter com a lista que esta vindo do state
   }
 
   private fun addingDividerDecoration() { //Função para implementar as linhas divisórias de tarefas do app
