8cc1c28cfa368da7e569e9c1182a067b7bc0c285
Daniele Stein <danieleastein@gmail.com>
2023-08-18 15:56:32 -0300

    Excluída a função addRandowHabit, e criada a função addHabit

--- app/src/main/java/com/lucasmontano/carreiranomundomobile/core/HabitsRepository.kt
+++ app/src/main/java/com/lucasmontano/carreiranomundomobile/core/HabitsRepository.kt
@@ -7,8 +7,8 @@ interface HabitsRepository {
 
   fun fetchHabits(): List<HabitItem>  //função que vai retornar a lista de habitos
 
-  fun addRandomNewHabit() //adicionar o novo hábito, habitos randomicos, pois ainda não foi criada a interface de cadastro
-
   fun toggleHabitCompleted(id: String) //função para marcar um hábito específico como concluído, passando o id do hábito
+
+  fun addHabit(name: String, habitDaySelected: List<Int>)
 
 }
